606a4bc6dc62c4488975a653b2bbb35ca447db98
jjy <jjyruby@gmail.com>
2024-10-11 21:13:25 +0800

    fix examples

--- examples/non_threadsafe_demo.rs
+++ examples/non_threadsafe_demo.rs
@@ -4,17 +4,21 @@ const FAST_HEAP_SIZE: usize = 32 * 1024; // 32 KB
 const HEAP_SIZE: usize = 1024 * 1024; // 1M
 const LEAF_SIZE: usize = 16;
 
-pub static mut FAST_HEAP: [u8; FAST_HEAP_SIZE] = [0u8; FAST_HEAP_SIZE];
-pub static mut HEAP: [u8; HEAP_SIZE] = [0u8; HEAP_SIZE];
+#[repr(align(64))]
+struct Heap<const S: usize>([u8; S]);
+
+static mut FAST_HEAP: Heap<FAST_HEAP_SIZE> = Heap([0u8; FAST_HEAP_SIZE]);
+static mut HEAP: Heap<HEAP_SIZE> = Heap([0u8; HEAP_SIZE]);
 
 // This allocator can't work in tests since it's non-threadsafe.
 #[cfg_attr(not(test), global_allocator)]
 static ALLOC: NonThreadsafeAlloc = unsafe {
-    let fast_param = FastAllocParam::new(FAST_HEAP.as_ptr(), FAST_HEAP_SIZE);
-    let buddy_param = BuddyAllocParam::new(HEAP.as_ptr(), HEAP_SIZE, LEAF_SIZE);
+    let fast_param = FastAllocParam::new(FAST_HEAP.0.as_ptr(), FAST_HEAP_SIZE);
+    let buddy_param = BuddyAllocParam::new(HEAP.0.as_ptr(), HEAP_SIZE, LEAF_SIZE);
     NonThreadsafeAlloc::new(fast_param, buddy_param)
 };
 
+#[allow(clippy::useless_vec)]
 fn main() {
     let v = vec![0u8; 42];
     let msg = "alloc success".to_string();
